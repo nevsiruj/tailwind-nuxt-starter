6ee4035e7af599d14e66c73d570d8fe3fdbfdc33
nevsiruj <ngonzalosuarez@gmail.com>
2024-01-27 16:49:09 -0300

    last

--- pages/index.vue
+++ pages/index.vue
@@ -140,45 +140,8 @@
     </svg>
 </a>
 
+<Footer />
 
-
-
-<footer class="bg-gray-900 text-white">
-      <div class="container mx-auto py-8 px-6">
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-          <div class="md:col-span-2">
-            <h3 class="text-2xl font-bold mb-4">Sobre nosotros</h3>
-            <p class="mb-4">
-              Somos una consultora de software especializada en soluciones a medida.
-
-            </p>
-          </div>
-          <div>
-            <h3 class="text-2xl font-bold mb-4">Contáctanos</h3>
-            <ul class="mb-4">
-              <li>
-                <i class="fas fa-map-marker-alt mr-2"></i>XXX, Info
-              </li>
-              <li><i class="fas fa-phone-alt mr-2"></i>+5492664255928</li>
-              <li><i class="fas fa-envelope mr-2"></i>info@vylaris.com.ar</li>
-            </ul>
-            <h3 class="text-2xl font-bold mb-4">Síguenos</h3>
-            <div class="flex">
-              <a href="https://www.facebook.com/vylaris" class="mr-4"><i class="fab fa-facebook fa-2x"></i></a>
-              <a href="https://www.instagram.com/vylaris.online/" class="mr-4"><i class="fab fa-instagram fa-2x"></i></a>
-              <!-- <a href="#" class="mr-4"><i class="fab fa-linkedin fa-2x"></i></a> -->
-            </div>
-          </div>
-        </div>
-        <hr class="border-gray-600 my-8" />
-        <div class="text-center">
-          <p class="text-gray-500 mb-4">
-            © 2024 Vylaris. Todos los derechos reservados.
-          </p>
-          <!-- <a href="#" class="text-purple-500 font-bold hover:underline">Política de privacidad</a> -->
-        </div>
-      </div>
-    </footer>
 
 
 </template>
@@ -189,6 +152,7 @@ import Header from './layout/header.vue';
 import Banner from './layout/banner.vue';
 import Categories from './layout/categories.vue';
 import Destacados from './layout/destacados.vue';
+import Footer from './layout/footer.vue';
 </script>
 
 <style>
---
+++ pages/layout/footer.vue
@@ -0,0 +1,40 @@
+<template>
+
+<footer class="bg-gray-900 text-white">
+      <div class="container mx-auto py-8 px-6">
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <div class="md:col-span-2">
+            <h3 class="text-2xl font-bold mb-4">Sobre nosotros</h3>
+            <p class="mb-4">
+              Somos una consultora de software especializada en soluciones a medida.
+
+            </p>
+          </div>
+          <div>
+            <h3 class="text-2xl font-bold mb-4">Contáctanos</h3>
+            <ul class="mb-4">
+              <li>
+                <i class="fas fa-map-marker-alt mr-2"></i>XXX, Info
+              </li>
+              <li><i class="fas fa-phone-alt mr-2"></i>+5492664255928</li>
+              <li><i class="fas fa-envelope mr-2"></i>info@vylaris.com.ar</li>
+            </ul>
+            <h3 class="text-2xl font-bold mb-4">Síguenos</h3>
+            <div class="flex">
+              <a href="https://www.facebook.com/vylaris" class="mr-4"><i class="fab fa-facebook fa-2x"></i></a>
+              <a href="https://www.instagram.com/vylaris.online/" class="mr-4"><i class="fab fa-instagram fa-2x"></i></a>
+              <!-- <a href="#" class="mr-4"><i class="fab fa-linkedin fa-2x"></i></a> -->
+            </div>
+          </div>
+        </div>
+        <hr class="border-gray-600 my-8" />
+        <div class="text-center">
+          <p class="text-gray-500 mb-4">
+            © 2024 Vylaris. Todos los derechos reservados.
+          </p>
+          <!-- <a href="#" class="text-purple-500 font-bold hover:underline">Política de privacidad</a> -->
+        </div>
+      </div>
+    </footer>
+
+</template>
